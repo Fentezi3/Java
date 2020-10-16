9b7ad9fc1e8d66a53e2f483eab08363451cb8ea8
Kseniya <42131630+Fentezi3@users.noreply.github.com>
2020-10-16 14:48:31 +0300

    Revert "Feature (#2)" (#3)
    This reverts commit 7a34f3a987dae18064b0aafe87adeb435c23e650.

--- src/main/java/com/example/MainTask1.java
+++ src/main/java/com/example/MainTask1.java
@@ -1,15 +1,14 @@
-//1.Приветствовать любого пользователя при вводе его имени через командную строку.
-
 package com.example;
-
 import java.util.Scanner;
 
 public class MainTask1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce yourself:");
-        String name = scanner.nextLine();
-        scanner.close();
-        System.out.printf("Hello, %s\n", name);
+    static class Test {
+        public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Introduce yourself: ");
+            String name = in.nextLine();
+            System.out.printf("Hello, %s", name);
+            in.close();
+        }
     }
 }
