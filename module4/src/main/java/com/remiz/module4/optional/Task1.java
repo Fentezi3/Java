a045d77d0c812c2840053436a07714fb218541d8
Kseniya <42131630+Fentezi3@users.noreply.github.com>
2020-11-20 18:31:01 +0300

    Console app for main task in module4 (#12)
    * Console app for main task in module4
    
    * Some fixes in module4
    
    * Main task and otional tasks for modul4
    
    * Mistakes correction
    
    * Changes in optional tasks
    
    Co-authored-by: Kseniya Ramizonava <kseniya_ramizonava@epam.com>

---
+++ module4/src/main/java/com/remiz/module4/optional/Task1.java
@@ -0,0 +1,31 @@
+package com.remiz.module4.optional;
+
+import com.remiz.module4.scan.ScannerWrapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        try (Scanner scanner = new Scanner(new File("src/main/resources/string.txt"));
+             FileWriter writer = new FileWriter(new File("src/main/resources/output.txt"))) {
+            System.out.println("We're scanning and reading information from file.\n");
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            for (int i = list.size() - 1; i >= 0; i--) {
+                writer.write(list.get(i) + "\n");
+            }
+            System.out.println("We're reversing order all lines in file.\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("All lines in reverse order in output.txt file.\n");
+    }
+}
