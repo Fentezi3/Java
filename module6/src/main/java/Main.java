89feaf680d6606e69da78e0c9b856d70a571f040
Kseniya Ramizonava <kseniya_ramizonava@epam.com>
2020-11-20 18:17:37 +0300

    Module6 task

--- module6/src/main/java/Main.java
+++ module6/src/main/java/Main.java
@@ -1,50 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    private final static String DIR_PREFIX = "|----";
+    private final static String FILE_PREFIX = "|   ";
+    private final static String DEFAULT_INDENTATION = "   ";
+
     public static void main(String[] args) {
         try {
             Writer writer = new FileWriter("src/main/resources/outputRammTree.txt");
             printTree(0, new File("d:/Rammstein"), writer);
             writer.close();
+            List<String> stringList = readFromFile();
+            calculateDirectories(stringList);
+            calculateFiles(stringList);
+            calculateAverageTitlesLength(stringList);
+            calculateAverageFilesAmountInDirectory(stringList);
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            readFromFile();
-        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     static void printTree(int depth, File file, Writer writer) throws IOException {
-        StringBuilder indent = new StringBuilder();
-        StringBuilder indentMinus = new StringBuilder();
-        StringBuilder intendLine = new StringBuilder();
+        StringBuilder indentation = new StringBuilder();
         File[] files;
-        int length = 4;
         for (int i = 0; i < depth; i++) {
-            indent.append(" ");
-            indentMinus.append("-");
-        }
-        for (int i = 0; i < depth - length; i++) {
-            intendLine.append(" ");
+            indentation.append(DEFAULT_INDENTATION);
         }
         if (file.isDirectory()) {
-            writer.write( indent.toString() + "|" +indentMinus.toString() + file.getName() + "\n");
-//            System.out.print("|" + indent.toString() + file.getName() + "\n");
-        } else{
-            writer.write( intendLine +"|  " + file.getName() + "\n");
-
+            writer.write(indentation + DIR_PREFIX + file.getName() + "\n");
+            System.out.print(indentation + DIR_PREFIX + file.getName() + "\n");
+        } else {
+            writer.write(indentation + FILE_PREFIX + file.getName() + "\n");
+            System.out.print(indentation + FILE_PREFIX + file.getName() + "\n");
         }
         if (file.isDirectory()) {
             files = file.listFiles();
             sortFileWrappers(Arrays.asList(files));
             for (File value : files) {
-                printTree(depth + length, value, writer);
+                printTree(depth + 1, value, writer);
             }
         }
     }
+
     private static void sortFileWrappers(List<File> childFiles) {
         childFiles.sort((file1, file2) -> {
             if ((file1.isFile() && file2.isFile())
@@ -60,24 +66,63 @@ public class Main {
             return 0;
         });
     }
-    public static void readFromFile() throws FileNotFoundException {
-        Queue <String> allTitles = new ArrayDeque<>();
-        List<String> stringList = new ArrayList<>();
-        int directionCheck = 0;
-        Scanner scanner = new Scanner(new File("src/main/resources/outputRammTree.txt"));
-        while (scanner.hasNextLine()){
-            allTitles.offer(scanner.nextLine());
-        }
-        for (String name : allTitles){
-            if(name.lastIndexOf("|--") > 0){
-                directionCheck++;
+
+    public static List<String> readFromFile() throws IOException {
+        return Files.lines(Paths.get("src/main/resources/outputRammTree.txt")).collect(Collectors.toList());
+    }
+
+    private static void calculateDirectories(List<String> stringList) {
+        int directories = (int) stringList.stream()
+                .filter(s -> s.contains(DIR_PREFIX)).count();
+        System.out.println("Directories amount: " + directories);
+    }
+
+    private static void calculateFiles(List<String> stringList) {
+        int files = (int) stringList.stream()
+                .filter(s -> s.contains(FILE_PREFIX)).count();
+        System.out.println("Files amount: " + files);
+    }
+
+    private static void calculateAverageTitlesLength(List<String> stringList) {
+        List<Integer> list = new ArrayList<>();
+        float sum = 0;
+        float average;
+        for (String string : stringList) {
+            if (string.contains(FILE_PREFIX)) {
+                Integer length = string.length() - FILE_PREFIX.length();
+                list.add(length);
             }
-            else {
-                stringList.add(name);
+        }
+        for (int length : list) {
+            sum += length;
+        }
+        average = sum / list.size();
+        System.out.println("Average file name length: " + average);
+    }
+
+    private static void calculateAverageFilesAmountInDirectory(List<String> stringList) {
+        List<Integer> counters = new ArrayList<>();
+        float sum = 0;
+        int fileCounter = 0;
+        for (String line : stringList) {
+            if (line.contains(DIR_PREFIX)) {
+                counters.add(fileCounter);
+                fileCounter = 0;
+            }
+            if (line.contains(FILE_PREFIX)) {
+                fileCounter++;
             }
         }
-        System.out.println(directionCheck);
-//        System.out.println(stringList);
-        scanner.close();
+        if (fileCounter > 0){
+            counters.add(fileCounter);
+        }
+        for (int counter : counters) {
+            sum += counter;
+        }
+        long withoutNull = counters.stream().
+                filter(integer -> integer > 0).count();
+        float average = sum / withoutNull;
+        System.out.println("Average files in directory: " + average);
+        System.out.println("Directions: " + withoutNull);
     }
 }
